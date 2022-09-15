b35c1e27c7d7b44ef5b9b09442ec69bf2a7c7509
jose rincon <joseinter@gmail.com>
2022-09-15 10:35:33 -0500

    mi primer commit

---
+++ src/proyectopracticag5/ProyectoPracticaG5.java
@@ -0,0 +1,16 @@
+package proyectopracticag5;
+
+/**
+ *
+ * @author Jose Rincon
+ */
+public class ProyectoPracticaG5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
