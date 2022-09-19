2b72929b6cb5827a3ea8037e06b93cbea13e25d2
jose rincon <joseinter@gmail.com>
2022-09-19 10:09:07 -0500

    agregar

--- src/Ejercicio1.java
+++ src/Ejercicio1.java
@@ -16,6 +16,7 @@ public class Ejercicio1 {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("hola estoy escribiendo un codigo para subirlo en git");
+        String persona = "Carlos";
                 
     }
     
